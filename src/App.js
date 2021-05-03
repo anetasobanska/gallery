4fea67050a5290f5280997bf207e166c0f907bc8
anetasobanska <sannettka@gmail.com>
2021-05-03 13:18:05 +0200

    create react app, styling gallery

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <>
+     <h1 style={{textAlign: 'center'}}>Jean Herve Roux</h1>
+     <h2 style={{textAlign: 'center'}}>prensentation</h2>
+     <Gallery />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './gallery.css';
+import Img1 from './img/img1.jpg';
+import Img2 from './img/img2.jpg';
+import Img3 from './img/img3.jpg';
+import Img4 from './img/img4.jpg';
+import Img5 from './img/img5.jpg';
+import Img6 from './img/img6.jpg';
+
+const Gallery = () => {
+
+    let data = [
+        {
+            id: 1,
+            imgSrc: Img1,
+        },
+        {
+            id: 2,
+            imgSrc: Img2,
+        },
+        {
+            id: 3,
+            imgSrc: Img3,
+        },
+        {
+            id: 4,
+            imgSrc: Img4,
+        },
+        {
+            id: 5,
+            imgSrc: Img5,
+        },
+        {
+            id: 6,
+            imgSrc: Img6,
+        }
+    ]
+
+    return(
+        <>
+            <div className="gallery">
+                {data.map((item, index)=>{
+                    return(
+                        <div className="pics" key={index}>
+                            <img src={item.imgSrc} alt={item.imgSrc} style={{width:'100%'}}/>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
+    )    
+}
+
+export default Gallery
