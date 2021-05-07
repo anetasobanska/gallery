8192486ac6fa7627e91803775d389c055ab30bf4
anetasobanska <sannettka@gmail.com>
2021-05-07 12:09:45 +0200

    favicon

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Gallery from './Gallery';
 function App() {
   return (
     <>
-     <h1 style={{textAlign: 'center'}}>Jean Herve Roux</h1>
-     <h2 style={{textAlign: 'center'}}>prensentation</h2>
+     <h1 style={{textAlign: 'left', marginLeft: '12px'}}>Jean Herve Roux</h1>
      <Gallery />
     </>
   );
