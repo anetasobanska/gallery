c417c4043c9e9ae45829fd50b0c1451029f77487
anetasobanska <sannettka@gmail.com>
2021-05-11 10:11:57 +0200

    emailjs added

--- src/Contact.js
+++ src/Contact.js
@@ -1,27 +1,31 @@
 import React , {useEffect} from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import emailjs from 'emailjs-com';
 import './Contact.css';
 
 const Contact = () => {
-    useEffect(() => {
-        AOS.init({
-            duration: 2000,
-            delay: 50
-        },);
-    }, [])
+    function sendEmail(e){
+        e.preventDefault();
+
+    emailjs.sendForm('service_t0q14mq', 'template_e31z9ud', e.target, 'user_4spjTWDT28LBJIviz1mry')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+    }
+
     return (
-        <div className="container" data-aos={"zoom-in"}>
-            <form className="contact-form">
-                <input type="hidden" name="contact_number" />
+        <div className="container">
+            <form onSubmit={sendEmail} className="contact-form">
                 <label>Nom</label>
-                <input type="text" name="user_name" />
+                <input type="text" name="from_name" />
                 <label>Email</label>
-                <input type="email" name="user_email" />
+                <input type="email" name="reply_to" />
                 <label>Message</label>
                 <textarea name="message" cols="30" rows="10" />
                 <div className="center">
-                    <input type="submit" value="Send" />
+                    <input type="submit" value="Envoyer" />
                 </div>
             </form>
         </div>
